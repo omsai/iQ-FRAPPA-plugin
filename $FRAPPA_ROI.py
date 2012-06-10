5f73938d415b1357f6ceb949d8cfca5215c1c55c
Pariksheet Nanda <pariksheet.nanda@gmail.com>
2012-06-10 01:27:47 -0400

    fix line endings in NPP

--- $FRAPPA_ROI.py
+++ $FRAPPA_ROI.py
@@ -1,2 +1,44 @@
-#SHELL"""Draws bitmap ROIs from each event onto duplicated image."""
-from imagedisk import iQImageDiskfrom iQImage import *from PIL import Image, ImageDrawfrom numpy import array# User editable variablesintensity = 16000line_width = 2# iQImage.shape seems to be the only way to get image width and height# attributes.  Monkey patch with explicit width and height properties, inspired# by ImageJ getWidth and getHeightdef _get_width(self):    return self.shape[-2]def _get_height(self):    return self.shape[-1]iQImage.getWidth = _get_widthiQImage.getHeight = _get_height# Get image from userfrom imagedisk import iQImageDiskid = iQImageDisk()# FIXME: Use GUI to allow user to choose imageim = iQImage(id, 'frap2.tif')# Assemble ROIs using PIL Imagefor roi in im.targeted_ROIs():    x = im.getWidth()    y = im.getHeight()    pil_im = Image.new('L', (x, y), 0)    if roi['type'] == 'Rectangle':        ImageDraw.Draw(pil_im).rectangle(roi['coordinates'],                               # FIXME: outline > 1 does not increase thickness                                         outline=line_width)    from iqtools.mplot import imshow    imshow(pil_im)##im2 = im.duplicate(im.title + '_drawEventROIs')
+#SHELL
+"""
+Draws bitmap ROIs from each event onto duplicated image.
+"""
+from imagedisk import iQImageDisk
+from iQImage import *
+from PIL import Image, ImageDraw
+from numpy import array
+
+# User editable variables
+intensity = 16000
+line_width = 2
+
+# iQImage.shape seems to be the only way to get image width and height
+# attributes.  Monkey patch with explicit width and height properties, inspired
+# by ImageJ getWidth and getHeight
+def _get_width(self):
+    return self.shape[-2]
+
+def _get_height(self):
+    return self.shape[-1]
+
+iQImage.getWidth = _get_width
+iQImage.getHeight = _get_height
+
+# Get image from user
+from imagedisk import iQImageDisk
+id = iQImageDisk()
+# FIXME: Use GUI to allow user to choose image
+im = iQImage(id, 'frap2.tif')
+
+# Assemble ROIs using PIL Image
+for roi in im.targeted_ROIs():
+    x = im.getWidth()
+    y = im.getHeight()
+    pil_im = Image.new('L', (x, y), 0)
+    if roi['type'] == 'Rectangle':
+        ImageDraw.Draw(pil_im).rectangle(roi['coordinates'],
+                               # FIXME: outline > 1 does not increase thickness
+                                         outline=line_width)
+    from iqtools.mplot import imshow
+    imshow(pil_im)
+
+##im2 = im.duplicate(im.title + '_drawEventROIs')
--- gui.py
+++ gui.py
@@ -1,1 +1,73 @@
-"""Qt Form for selecting iQImage"""import sysfrom PyQt4.QtCore import *from PyQt4.QtGui import *class ImageListWindow(QDialog):    def __init__(self, parent=None):        super(ImageListWindow, self).__init__(parent)        try:            from imagedisk import iQImageDisk            id = iQImageDisk()            names = id.titles()        except ImportError:            names = ['frappa pa405',                     'micropoint 365',                     'mosaic gfp']        # window title        self.setWindowTitle("Select iQ Image")        # view widget        self.listView = QListView()        self.sourceModel = QStandardItemModel(len(names), 1)        i = 0        for name in names:            item = QStandardItem(name)            self.sourceModel.setItem(i, item)            i += 1        self.proxyModel = QSortFilterProxyModel()        self.proxyModel.setSourceModel(self.sourceModel)        self.listView.setModel(self.proxyModel)        # filter widgets        self.filterLabel = QLabel("Find:")        self.filterLineEdit = QLineEdit()        # set layout        grid = QGridLayout()        grid.addWidget(self.filterLabel, 0, 0)        grid.addWidget(self.filterLineEdit, 0, 1)        grid.addWidget(self.listView, 1, 1)        self.setLayout(grid)        # set initial state        self.filterLineEdit.setText("")        # signals        self.connect(self.filterLineEdit,                     SIGNAL("textChanged(const QString&)"), self.filterList)    def filterList(self):        for index in range(self.proxyModel.rowCount()):            print self.sourceModel.data(self.proxyModel.index(index, 0))        regExp = QRegExp(self.filterLineEdit.text(),                                Qt.CaseInsensitive, QRegExp.Wildcard)        self.proxyModel.setFilterRegExp(regExp)    def submit(self):        print("clicked!")if __name__ == '__main__':    app = QApplication(sys.argv)    imageListWindow = ImageListWindow()    imageListWindow.show()    app.exec_()
+"""
+Qt Form for selecting iQImage
+"""
+
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+
+class ImageListWindow(QDialog):
+
+    def __init__(self, parent=None):
+        super(ImageListWindow, self).__init__(parent)
+
+        try:
+            from imagedisk import iQImageDisk
+            id = iQImageDisk()
+            names = id.titles()
+        except ImportError:
+            names = ['frappa pa405',
+                     'micropoint 365',
+                     'mosaic gfp']
+
+        # window title
+        self.setWindowTitle("Select iQ Image")
+
+        # view widget
+        self.listView = QListView()
+        self.sourceModel = QStandardItemModel(len(names), 1)
+        i = 0
+        for name in names:
+            item = QStandardItem(name)
+            self.sourceModel.setItem(i, item)
+            i += 1
+        self.proxyModel = QSortFilterProxyModel()
+        self.proxyModel.setSourceModel(self.sourceModel)
+        self.listView.setModel(self.proxyModel)
+
+        # filter widgets
+        self.filterLabel = QLabel("Find:")
+        self.filterLineEdit = QLineEdit()
+
+        # set layout
+        grid = QGridLayout()
+        grid.addWidget(self.filterLabel, 0, 0)
+        grid.addWidget(self.filterLineEdit, 0, 1)
+        grid.addWidget(self.listView, 1, 1)
+        self.setLayout(grid)
+
+        # set initial state
+        self.filterLineEdit.setText("")
+
+        # signals
+        self.connect(self.filterLineEdit,
+                     SIGNAL("textChanged(const QString&)"), self.filterList)
+
+    def filterList(self):
+        for index in range(self.proxyModel.rowCount()):
+            print self.sourceModel.data(self.proxyModel.index(index, 0))
+
+        regExp = QRegExp(self.filterLineEdit.text(),
+                                Qt.CaseInsensitive, QRegExp.Wildcard)
+        self.proxyModel.setFilterRegExp(regExp)
+
+    def submit(self):
+        print("clicked!")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    imageListWindow = ImageListWindow()
+    imageListWindow.show()
+    app.exec_()
